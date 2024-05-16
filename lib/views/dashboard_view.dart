5420d7e7891b4f64dc0a53f981c74f04ffe13579
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2024-05-17 00:33:33 +0300

    Finish Mobile layout

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -3,15 +3,42 @@ import 'package:responsive_dashboard/views/widgets/adaptive_layout.dart';
 import 'package:responsive_dashboard/views/widgets/dashboard_desktop_layout.dart';
 import 'package:responsive_dashboard/views/widgets/dashboard_tablet_layout.dart';
 
-class DashboardView extends StatelessWidget {
+import 'widgets/custom_drawer.dart';
+import 'widgets/dashboard_mobile_layout.dart';
+
+class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
+
+  @override
+  State<DashboardView> createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  final GlobalKey<ScaffoldState> scaffoldState = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldState,
+      drawer:
+          MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
+      appBar: MediaQuery.sizeOf(context).width < 800
+          ? AppBar(
+              backgroundColor: const Color(0Xff4EB7F2),
+              leading: IconButton(
+                onPressed: () {
+                  scaffoldState.currentState!.openDrawer();
+                },
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+              ),
+            )
+          : null,
       backgroundColor: const Color(0xffF7F9FA),
       body: AdaptiveLayout(
-        mobileLayout: (context) => const SizedBox(),
+        mobileLayout: (context) => const DashboardMobileLayout(),
         tabletLayout: (context) => const DashboardTabletLayout(),
         desktopLayout: (context) => const DashboardDesktopLayout(),
       ),
